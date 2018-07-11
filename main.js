8b763c8b495de555edc165e8d3b204689c2bc830
Kevin <34873229+RogerTheRabbit@users.noreply.github.com>
2018-07-11 15:54:20 -0400

    Changed regex
    The old regular expression would select every pair of digits in a number resulting in 10 being turned into 1 * 0 = 0.
    Hopefully, the new regex didn't break anything else.

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ function deleteEquation(id) {
 function eqParser(eq) {
 
     //Regular expressions are so cool
-    var pattern = /([x\d][\w\(])|(\)[\d\w])/;
+    var pattern = /([x\d][x\(])|(\)[\w])/;
     while (eq.search(pattern) != -1) {
         eq = eq.substring(0, eq.search(pattern) + 1) + "*" + eq.substring(eq.search(pattern) + 1);
     }
